e7ff78d4ff0030baed75959d938b9e423c9730e2
Marco Antonio Gonzalez Junior <m@rco.sh>
2020-04-18 21:29:06 -0300

    more api

--- services/web/project/models.py
+++ services/web/project/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
+from dataclasses import dataclass
 from project import app
 
 
@@ -11,8 +12,12 @@ manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
 
+@dataclass
 class Category(db.Model):
     __tablename__ = "categories"
+    id: int
+    name: str
+    
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(80), unique = True, nullable = False)
 
--- services/web/project/routes.py
+++ services/web/project/routes.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+from flask import jsonify, request
 from project import app
 from project.models import Category
 
@@ -9,13 +9,16 @@ def healthcheck():
 
 
 @app.route('/api/categories')
-def list_categories():
-    return jsonify(Category.query.all())
-
+def categories():
+    if request.method == 'GET':
+        return jsonify(Category.query.all())
+    else:
+        return "Fudeu"
 
 @app.route('/api/categories/<int:category_id>')
 def find_category(category_id):
-    return 'Category %d' % category_id
+    # return 'Category %d' % category_id
+    return jsonify(Category.query.filter_by(id = category_id).first())
 
 
 @app.route('/api/categories/<int:category_id>/products')
